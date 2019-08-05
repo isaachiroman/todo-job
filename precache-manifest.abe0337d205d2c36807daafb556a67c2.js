cb8f9048a4ca1d8c8390e7e6b570f01a8a339382
isaachiroman <isaachiroman@gmail.com>
2019-08-05 16:45:12 +0700

    Updates

---
+++ precache-manifest.abe0337d205d2c36807daafb556a67c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4961f64b29208cb467b8d8ea940bc4aa",
+    "url": "/todo-job/index.html"
+  },
+  {
+    "revision": "32edf6de98cfc06a319b",
+    "url": "/todo-job/static/css/main.6816df39.chunk.css"
+  },
+  {
+    "revision": "774d5e3722e17f42d013",
+    "url": "/todo-job/static/js/2.cba05053.chunk.js"
+  },
+  {
+    "revision": "32edf6de98cfc06a319b",
+    "url": "/todo-job/static/js/main.c7b3607a.chunk.js"
+  },
+  {
+    "revision": "62c44f75422cce299b26",
+    "url": "/todo-job/static/js/runtime~main.0f51022e.js"
+  }
+]);
